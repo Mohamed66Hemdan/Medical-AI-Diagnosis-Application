c4cc95f7e477a32f50b4d908cb7d4d4f212c6c5f
Mohamed Hemdan <102409507+Mohamed66Hemdan@users.noreply.github.com>
2025-09-04 16:19:21 +0300

    Update Medical.py

--- pages/Medical.py
+++ pages/Medical.py
@@ -122,17 +122,10 @@ def binary_transform(X):
     })
 binary_transformer = FunctionTransformer(binary_transform)
 ###########################################
-# liver_model = joblib.load("xgb_pipeline_with_le.pkl")
-# pipeline = liver_model["pipeline"]
-# le = liver_model["label_encoder"]
-@st.cache_resource
-def load_model(path="xgb_pipeline_with_le.pkl"):
-    liver_model = joblib.load(path)
-    pipeline = liver_model["pipeline"]
-    le = liver_model["label_encoder"]
-    return pipeline, le
+liver_model = joblib.load("xgb_pipeline_with_le.pkl")
+pipeline = liver_model["pipeline"]
+le = liver_model["label_encoder"]
 
-pipeline, le = load_model()
 ###########################################
 ###########################################
 # Model 3
@@ -356,6 +349,7 @@ with tab3:
                 unsafe_allow_html=True
             )
 # 
+
 
 
 
